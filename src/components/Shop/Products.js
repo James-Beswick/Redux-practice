51eeaf8aaf34423242755da446f0fb43988659c1
James Beswick <james@192.168.1.139>
2021-08-05 17:03:57 +0100

    added functionality to addToCart

--- src/components/Shop/Products.js
+++ src/components/Shop/Products.js
@@ -1,16 +1,35 @@
 import ProductItem from './ProductItem';
 import classes from './Products.module.css';
 
+const DUMMY_PRODUCTS = [
+  {
+    id: 'p1',
+    price: 6,
+    name: 'my first book',
+    description: 'a book, you shall see',
+  },
+  {
+    id: 'p2',
+    price: 10,
+    name: 'my second book',
+    description: 'again, a book. see, you shall',
+  },
+];
+
 const Products = props => {
   return (
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
       <ul>
-        <ProductItem
-          title="Test"
-          price={6}
-          description="This is a first product - amazing!"
-        />
+        {DUMMY_PRODUCTS.map(product => (
+          <ProductItem
+            key={product.id}
+            id={product.id}
+            title={product.name}
+            price={product.price}
+            description={product.description}
+          />
+        ))}
       </ul>
     </section>
   );

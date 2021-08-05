0e6ab1355dde556d8d60d653606f89bd7a39cc7d
James Beswick <james@192.168.1.139>
2021-08-05 15:12:23 +0100

    back to the start to retry

--- src/components/Cart/CartButton.js
+++ src/components/Cart/CartButton.js
@@ -1,13 +1,10 @@
-import { useSelector } from 'react-redux';
 import classes from './CartButton.module.css';
 
 const CartButton = props => {
-  const totalItems = useSelector(state => state.totalItems);
-
   return (
     <button className={classes.button}>
       <span>My Cart</span>
-      <span className={classes.badge}>{totalItems}</span>
+      <span className={classes.badge}>1</span>
     </button>
   );
 };
